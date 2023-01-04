d70b321ded2be32af9ed3512f2e60d7051d0812a
Sandro Costa <sandro.nbs@gmail.com>
2023-01-04 14:08:43 +0000

    fix: add getset for player health

--- src/scripts/GameState.gd
+++ src/scripts/GameState.gd
@@ -1,6 +1,14 @@
 extends Node
 #Global variables and game state
 
-var player_hp: int = 80
-var player_max_hp: int = 100
+var player_hp: int = 100
+var _player_max_hp: int = 100 setget set_max_hp, get_max_hp
 var wave: int = 1
+var enemies_killed: int = 0
+var spawn_num_enemies: int = 5
+
+func set_max_hp(max_hp: int):
+	_player_max_hp = max_hp
+
+func get_max_hp():
+	return _player_max_hp
--- src/scripts/PlayerHealthBar.gd
+++ src/scripts/PlayerHealthBar.gd
@@ -3,6 +3,6 @@ extends ProgressBar
 onready var LabelText = get_node("LabelHealth")
 
 func _ready():
-	self.max_value = GameState.player_max_hp
+	self.max_value = GameState.get_max_hp()
 	self.value = GameState.player_hp
-	LabelText.text = str(GameState.player_hp) + "/" + str(GameState.player_max_hp)
+	LabelText.text = str(GameState.player_hp) + "/" + str(GameState.get_max_hp())
